74d319b4fade46e3b8f9554fb4150e545e88fde6
wrona96 <git@github.com>
2018-08-27 11:09:18 +0200

    Refactor file to PEP8, added exception handling

--- port_scanner/scanner.py
+++ port_scanner/scanner.py
@@ -1,48 +1,53 @@
-import threading
-import socket
 import json
+import socket
+import threading
 from queue import Queue
 
-
 lib_ports = json.loads(open('ports.json').read())
-target = '192.168.0.1'
+target = 'w3c.pl'
 threads = 300
 rang = 1000
 
 try:
     ip = socket.gethostbyname(target)
     print('\nChecking: {target} as {ip}\n'.format(target=target, ip=ip))
-except:
+except IndentationError:
     print('This site does not exist!!!')
 
-def portscan_custom(port):
+
+def port_scan(port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(1)
+    s.settimeout(0.5)
     try:
-        con = s.connect((target,port))
+        s.connect((target, port))
         try:
             print('Port : {port} is open. ({desc})'.format(port=port, desc=lib_ports[str(port)]))
-        except:
+        except KeyError:
             print('Port : {port} is open.'.format(port=port))
-        con.close()
-    except:
+    except socket.timeout:
         pass
+    except socket.error:
+        pass
+    finally:
+        s.close()
+
 
 def threader():
     while True:
         worker = q.get()
-        portscan_custom(worker)
+        port_scan(worker)
         q.task_done()
+
 
 q = Queue()
 
-for x in range(threads):#Creating threads
+for x in range(threads):  # Creating threads
     t = threading.Thread(target=threader)
     t.daemon = True
     t.start()
-#for port, desc in lib_ports.items():#Znane Porty
-    #q.put(int(port))
-for worker in range(1,rang):#Wszystkie z zakresu
+# for port, desc in lib_ports.items():#Znane Porty
+# q.put(int(port))
+for worker in range(1, rang):
     q.put(worker)
 
 q.join()
